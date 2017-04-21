83489b921e879ebb5c923aaa0a48cdcd77ebcc4e
guojun <gjscut@qq.com>
2017-04-21 20:37:40 +0800

    add change password

--- server/handlers/user.js
+++ server/handlers/user.js
@@ -19,7 +19,7 @@ module.exports.get = function(request, response, next) {
  * Validate the POST request body and create a new Thing
  */
 module.exports.create = function(request, response, next) {
-  var newUser = new User(request.body);
+  const newUser = new User(request.body);
   newUser.password = utils.encrypt(newUser.password);
   return User
     .create(newUser)
@@ -27,4 +27,34 @@ module.exports.create = function(request, response, next) {
     .catch(error => next(error));
 }
 
+
+/**
+ * Validate the POST request body and create a new Thing
+ */
+module.exports.changePassword = function(request, response, next) {
+  const oldUser = new User(request.body);
+  const newPassword = utils.encrypt(request.body.newPassword);
+  User.findOne({username: oldUser.username}, function(err, user) {
+    if (err) { return next(err); }
+    if (!user) { return next(null, false, { message: 'Unknow User' }); }
+    if (!utils.validEncrypt(oldUser.password, user.password)) { return next(null, false); }
+    user.password = newPassword;
+    return user
+      .save()
+      .then(user => response.status(200).json(user))
+      .catch(error => next(error));
+  });
+}
+
+
+/**
+ * Validate the POST request body and create a new Thing
+ */
+module.exports.delete = function(request, response, next) {
+  const id = request.param.id;
+  return User
+    .remove({_id: id})
+    .then(user => response.status(200).json(user))
+    .catch(error => next(error));
+}
 
