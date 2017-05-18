69efbdbf9cf5fb71be2ac50a9bfd88f0bfd3c497
Guo Jun <guo_j@worksap.co.jp>
2017-05-18 18:39:19 +0800

    add chai

---
+++ server/tests/unit-tests.js
@@ -0,0 +1,93 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('getSearchResultCache(model, sort, skip, limit)', function() {
+  it('getSearchResultCache should return required limit size', function() {
+    expect(0).to.equal(0);
+  });
+});
+describe('getSearchResultCache(model, sort, skip, limit)', function() {
+  it('getSearchResultCache should return required limit size', function() {
+    expect(0).to.equal(0);
+  });
+});describe('getSearchResultCache(model, sort, skip, limit)', function() {
+  it('getSearchResultCache should return required limit size', function() {
+    expect(0).to.equal(0);
+  });
+});describe('getSearchResultCache(model, sort, skip, limit)', function() {
+  it('getSearchResultCache should return required limit size', function() {
+    expect(0).to.equal(0);
+  });
+});describe('getSearchResultCache(model, sort, skip, limit)', function() {
+  it('getSearchResultCache should return required limit size', function() {
+    expect(0).to.equal(0);
+  });
+});describe('getSearchResultCache(model, sort, skip, limit)', function() {
+  it('getSearchResultCache should return required limit size', function() {
+    expect(0).to.equal(0);
+  });
+});describe('getSearchResultCache(model, sort, skip, limit)', function() {
+  it('getSearchResultCache should return required limit size', function() {
+    expect(0).to.equal(0);
+  });
+});describe('getSearchResultCache(model, sort, skip, limit)', function() {
+  it('getSearchResultCache should return required limit size', function() {
+    expect(0).to.equal(0);
+  });
+});describe('getSearchResultCache(model, sort, skip, limit)', function() {
+  it('getSearchResultCache should return required limit size', function() {
+    expect(0).to.equal(0);
+  });
+});describe('getSearchResultCache(model, sort, skip, limit)', function() {
+  it('getSearchResultCache should return required limit size', function() {
+    expect(0).to.equal(0);
+  });
+});describe('getSearchResultCache(model, sort, skip, limit)', function() {
+  it('getSearchResultCache should return required limit size', function() {
+    expect(0).to.equal(0);
+  });
+});describe('getSearchResultCache(model, sort, skip, limit)', function() {
+  it('getSearchResultCache should return required limit size', function() {
+    expect(0).to.equal(0);
+  });
+});describe('getSearchResultCache(model, sort, skip, limit)', function() {
+  it('getSearchResultCache should return required limit size', function() {
+    expect(0).to.equal(0);
+  });
+});describe('getSearchResultCache(model, sort, skip, limit)', function() {
+  it('getSearchResultCache should return required limit size', function() {
+    expect(0).to.equal(0);
+  });
+});describe('getSearchResultCache(model, sort, skip, limit)', function() {
+  it('getSearchResultCache should return required limit size', function() {
+    expect(0).to.equal(0);
+  });
+});describe('getSearchResultCache(model, sort, skip, limit)', function() {
+  it('getSearchResultCache should return required limit size', function() {
+    expect(0).to.equal(0);
+  });
+});describe('getSearchResultCache(model, sort, skip, limit)', function() {
+  it('getSearchResultCache should return required limit size', function() {
+    expect(0).to.equal(0);
+  });
+});describe('getSearchResultCache(model, sort, skip, limit)', function() {
+  it('getSearchResultCache should return required limit size', function() {
+    expect(0).to.equal(0);
+  });
+});describe('getSearchResultCache(model, sort, skip, limit)', function() {
+  it('getSearchResultCache should return required limit size', function() {
+    expect(0).to.equal(0);
+  });
+});describe('getSearchResultCache(model, sort, skip, limit)', function() {
+  it('getSearchResultCache should return required limit size', function() {
+    expect(0).to.equal(0);
+  });
+});describe('getSearchResultCache(model, sort, skip, limit)', function() {
+  it('getSearchResultCache should return required limit size', function() {
+    expect(0).to.equal(0);
+  });
+});describe('getSearchResultCache(model, sort, skip, limit)', function() {
+  it('getSearchResultCache should return required limit size', function() {
+    expect(0).to.equal(0);
+  });
+});
